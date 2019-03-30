2098c634b09afaa065610e56f11ebd8b761afbde
Lucy20209060 <550502661@qq.com>
2019-03-30 22:49:32 +0800

    管理资源

--- demo-01/main.js
+++ demo-01/main.js
@@ -1,3 +1,5 @@
-import './index.css'
+import './index.css';
+// import img from './utils/xl.jpeg';
 
-document.querySelector('#app').innerText = 'demo-01'
+document.querySelector('#app').innerText = 'demo-01';
+// document.querySelector('.img').innerHTML = '<img src='+ img +' />'
--- demo-01/webpack.config.js
+++ demo-01/webpack.config.js
@@ -7,12 +7,20 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    rules: [{
-      test: /\.css$/,
-      use: [
-      'style-loader',
-      'css-loader'
-      ]
-    }]
+    rules: [
+      // 加载css文件
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif|jpeg)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      }
+    ]
   }
-};
+}
