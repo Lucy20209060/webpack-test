0252de6d1f35b95b82283fe9d965d1cc0ef5cf53
Lucy20209060 <550502661@qq.com>
2019-04-14 14:47:29 +0800

    webpack—TypeScript

---
+++ demo-12/src/index.ts
@@ -0,0 +1,8 @@
+const a:string = '123';
+
+function test(a: any): any {
+    return a
+}
+
+const b = test(a);
+console.log(b)
---
+++ demo-12/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    // new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new HtmlWebpackPlugin({
+      title: 'TypeScript'
+    })
+  ]
+}
